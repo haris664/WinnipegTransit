7d81f90d5b87bd840ac50823ae7a6894a792904e
Haris Munir <haris4000@yahoo.com>
2020-05-11 17:01:11 -0500

    Populating a search list that user enter

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const api = `zEE5nKn5wIFmy03pO7b`;
 const search = document.querySelector('form');
+const stEle = document.querySelector('.streets');
 
 search.onsubmit = event => {
   const input = event.target.querySelector('input');
@@ -17,6 +18,16 @@ function searchStreet(query) {
       }
     })
     .then(data => {
-      console.log(data)
+      streetDetails(data.streets);
     })
+}
+
+function streetDetails(street) {
+  if(street.length === 0) {
+    stEle.textContent = 'No Result Found';
+  }
+  street.forEach(st => {
+    stEle.insertAdjacentHTML(`afterbegin`,
+    `<a href="#" data-street-key=${st.key}>${st.name}</a>`)
+  });
 }
